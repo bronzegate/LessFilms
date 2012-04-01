487354e139f6f7cabb101c28d5291e7522c0d946
Josh Peters <bronzegateinc@gmail.com>
2012-04-01 17:19:44 -0500

    Video nav menu working on videos page

--- site/javascripts/menu_scripts.js
+++ site/javascripts/menu_scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  $(".vid_category").click(function(){
-    $(this).children("ul").slideToggle(200);
+  $(".vid_category ul").hide();
+  
+  $(".vid_category h3").click(function(){
+    if ($(this).parent().hasClass('closed')){
+      $(this).parent().children("ul").slideToggle(200);
+      $(this).parent().children(".triangle").rotate('90deg');
+      $(this).parent().removeClass('closed');
+      $(this).parent().addClass('opened');
+    }
+    else {
+      $(this).parent().children("ul").slideToggle(200);
+      $(this).parent().children(".triangle").rotate('0deg');
+      $(this).parent().removeClass('opened');
+      $(this).parent().addClass('closed');
+    }
   });
 });
