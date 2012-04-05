3ae9194d3a696675bd167e870ac21b994f803b39
Josh Peters <bronzegateinc@gmail.com>
2012-04-04 21:26:55 -0500

    Jquery cleanup

--- site/javascripts/site_intro.js
+++ site/javascripts/site_intro.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-	$("body").css('position', 'relative');
-	$("body").css('top', '100%');
-	$("body").css('overflow', 'hidden');
+	$("body").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'});
 	$("body").stop().animate({top: '0' }, {queue:false, duration:1000, easing:'easeOutBounce'});
 });
