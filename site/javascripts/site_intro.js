d4b351292ac1d43a82f223744bbcb782e094678f
Josh Peters <bronzegateinc@gmail.com>
2012-04-05 14:56:44 -0500

    Minor cleanup, latest build

--- site/javascripts/site_intro.js
+++ site/javascripts/site_intro.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
 	$("body").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'});
 	$("body").stop().animate({top: '0' }, {queue:false, duration:1000, easing:'easeOutBounce'});
+	$("body").css({'overflow' : 'auto'});
 });
