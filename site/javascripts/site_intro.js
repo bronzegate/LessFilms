228e4005369cd95970a5dbd547c2f532e749f4ab
Josh Peters <bronzegateinc@gmail.com>
2012-04-18 15:13:47 -0500

    Video working

--- site/javascripts/site_intro.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".lucha_home, .footer").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'}).stop();
-	$(".lucha_home, .footer").delay(4000).queue(function( nxt ) {	
-		$(this).animate({top: '0' }, {duration:1000, easing:'easeOutBounce'}).css({'overflow' : 'auto'});
-	nxt();
-	});
-});
