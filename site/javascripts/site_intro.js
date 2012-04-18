3e780b7b5e0fed02641018e21462fe2964c42612
Josh Peters <bronzegateinc@gmail.com>
2012-04-18 10:29:18 -0500

    Add video folder

--- site/javascripts/site_intro.js
+++ site/javascripts/site_intro.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("body").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'});
-	$("body").stop().animate({top: '0' }, {queue:false, duration:1000, easing:'easeOutBounce'});
-	$("body").css({'overflow' : 'auto'});
+	$(".lucha_home").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'});
+	$(".lucha_home").stop().animate({top: '0' }, {queue:false, duration:1000, easing:'easeOutBounce'});
+	$(".lucha_home").css({'overflow' : 'auto'});
 });
