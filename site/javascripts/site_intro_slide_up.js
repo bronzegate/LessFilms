42f8372131778928721a2138e00fad3b2c46a53a
Josh Peters <bronzegateinc@gmail.com>
2012-04-18 17:21:38 -0500

    Minor changes

--- site/javascripts/site_intro_slide_up.js
+++ site/javascripts/site_intro_slide_up.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	$(".lucha_home, .footer").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden'});
+	$(".lucha_home, .footer").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden', 'display' : 'none'});
 	$(".lucha_home, .footer").delay(4000).queue(function( nxt ) {
 		$(".intro_vid").animate({top: '-100%'}, {duration:350, easing:'linear'});	
-		$(this).animate({top: '0' }, {duration:1000, easing:'easeOutBounce'}).css({'overflow' : 'auto'});
+		$(this).css({'display' : 'block'}).animate({top: '0' }, {duration:1000, easing:'easeOutBounce'}).css({'overflow' : 'auto'});
 	nxt();
 	});
 });
