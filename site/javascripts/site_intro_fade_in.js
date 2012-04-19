536cef90fada43a00798e3a4c70e89d48069b5d7
Josh Peters <bronzegateinc@gmail.com>
2012-04-19 15:31:12 -0500

    Minor tweaks

--- site/javascripts/site_intro_fade_in.js
+++ site/javascripts/site_intro_fade_in.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-	$(".lucha_home, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
+	$(".lucha_home, .wrapper, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
 	$(".intro_vid").delay(4000).queue(function( frst ) {
 		$(this).animate({top: '100%'}, {duration:300, easing:'linear'});	
 	frst();
 	});
-	$(".lucha_home, .footer").delay(4300).queue(function( nxt ) {
+	$(".lucha_home").delay(4300).queue(function( nxt ) {
 		$(this).fadeIn(1000).css({'overflow' : 'auto'});
+		$(".wrapper, .footer").css({'display' : 'none'});
 	nxt();
+	});
+	$(".wrapper, .footer").delay(5300).queue(function( trd ) {
+		$(this).fadeIn(1000).css({'overflow' : 'auto'});
+	trd();
 	});
 });
