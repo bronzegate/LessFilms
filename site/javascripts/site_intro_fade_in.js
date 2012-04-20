990bee49f8343a93a1671d8f9b6417413d8a6e63
Josh Peters <bronzegateinc@gmail.com>
2012-04-20 12:03:46 -0500

    Add intro session cookie so video only plays once per session

--- site/javascripts/site_intro_fade_in.js
+++ site/javascripts/site_intro_fade_in.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
-	$(".lucha_home, .wrapper, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
-	$(".intro_vid").delay(4000).queue(function( frst ) {
-		$(this).animate({top: '100%'}, {duration:300, easing:'linear'});	
-	frst();
-	});
-	$(".lucha_home").delay(4300).queue(function( nxt ) {
-		$(this).fadeIn(1000).css({'overflow' : 'auto'});
-		$(".wrapper, .footer").css({'display' : 'none'});
-	nxt();
-	});
-	$(".wrapper, .footer").delay(4600).queue(function( trd ) {
-		$(this).fadeIn(1000).css({'overflow' : 'auto'});
-	trd();
-	});
+	if($.cookie("viewed_intro") == null)
+    {
+
+		$(".lucha_home, .wrapper, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
+		$(".intro_vid").delay(4000).queue(function( frst ) {
+			$(this).animate({top: '100%'}, {duration:300, easing:'linear'});	
+		frst();
+		});
+		$(".lucha_home").delay(4300).queue(function( nxt ) {
+			$(this).fadeIn(1000).css({'overflow' : 'auto'});
+			$(".wrapper, .footer").css({'display' : 'none'});
+		nxt();
+		});
+		$(".wrapper, .footer").delay(4600).queue(function( trd ) {
+			$(this).fadeIn(1000).css({'overflow' : 'auto'});
+		trd();
+		});
+		
+      	$.cookie('viewed_intro', true);
+    }
+    else
+    {
+    	$(".intro_vid").css({'display' : 'none'});
+    }
 });
