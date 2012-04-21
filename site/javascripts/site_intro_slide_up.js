f69e095a0f476cd2869f3e9d7d88978e02c2edac
Josh Peters <bronzegateinc@gmail.com>
2012-04-21 14:46:18 -0500

    Major Cleanup

--- site/javascripts/site_intro_slide_up.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".lucha_home, .footer").css({'position' : 'relative', 'top' : '100%', 'overflow' : 'hidden', 'display' : 'none'});
-	$(".lucha_home, .footer").delay(4000).queue(function( nxt ) {
-		$(".intro_vid").animate({top: '-100%'}, {duration:350, easing:'linear'});	
-		$(this).css({'display' : 'block'}).animate({top: '0' }, {duration:1000, easing:'easeOutBounce'}).css({'overflow' : 'auto'});
-	nxt();
-	});
-});
