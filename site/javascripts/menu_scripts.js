4d58551b4171af0efcdf33cea9847f6cbe91934b
Josh Peters <bronzegateinc@gmail.com>
2012-04-22 13:39:40 -0500

    Updated video page functionality

--- site/javascripts/menu_scripts.js
+++ site/javascripts/menu_scripts.js
@@ -1,18 +1,42 @@
-$(document).ready(function(){
-  $(".vid_category ul").hide();
+$(document).ready(function(){ 
+	$('.vid_nav .current').prepend('<div class="triangle" />');
   
-  $(".vid_category h3, .vid_category .triangle").click(function(){
-    if ($(this).parent().hasClass('closed')){
-      $(this).parent().children("ul").slideToggle(200);
-      $(this).parent().children(".triangle").rotate('90deg');
-      $(this).parent().removeClass('closed');
-      $(this).parent().addClass('opened');
-    }
-    else {
-      $(this).parent().children("ul").slideToggle(200);
-      $(this).parent().children(".triangle").rotate('0deg');
-      $(this).parent().removeClass('opened');
-      $(this).parent().addClass('closed');
-    }
-  });
+	$(".vid_category").click(function(){
+		$(".triangle").remove();
+		$(".vid_category").removeClass('current');
+		$(".vid_category").addClass('closed');
+		$(this).prepend('<div class="triangle" />');
+		$(this).removeClass('closed');
+		$(this).addClass('current');
+		
+		fadeOutSpeed = 1000
+		fadeInSpeed = 1000
+		
+		$(".vid_thumbs").fadeOut(fadeOutSpeed);
+		
+		if($(this).hasClass('vid_nav_promos')){
+			$('.vid_cat_promos').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_conferences')){
+			$('.vid_cat_conferences').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_documentaries')){
+			$('.vid_cat_documentaries').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_office_culture')){
+			$('.vid_cat_office_culture').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_web_commercials')){
+			$('.vid_cat_web_commercials').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_animation')){
+			$('.vid_cat_animation').fadeIn(fadeInSpeed);
+		}
+		if($(this).hasClass('vid_nav_motion_graphics')){
+			$('.vid_cat_motion_graphics').fadeIn(fadeInSpeed);
+		}
+	});
+
 });
+
+
