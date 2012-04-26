851082c426b16a10882ad0378480d262c73e9076
Josh Peters <bronzegateinc@gmail.com>
2012-04-26 14:08:56 -0500

    Intro updated

--- site/javascripts/site_intro_fade_in.js
+++ site/javascripts/site_intro_fade_in.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
     {
 
 		$(".lucha_home, .wrapper, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
-		$(".intro_vid").delay(4000).queue(function( frst ) {
-			$(this).animate({top: '100%'}, {duration:300, easing:'linear'});	
+		$(".intro_vid").delay(4600).queue(function( frst ) {
+			$(this).css({'display' : 'none'});	
 		frst();
 		});
-		$(".lucha_home").delay(4300).queue(function( nxt ) {
+		$(".lucha_home").delay(4900).queue(function( nxt ) {
 			$(this).fadeIn(1000).css({'overflow' : 'auto', 'background-position' : 'center 24px'});
 			$('#fancybox-overlay').css({'margin-top' : '0'});
 			$(".wrapper, .footer").css({'display' : 'none'});
 		nxt();
 		});
-		$(".wrapper, .footer").delay(4600).queue(function( trd ) {
+		$(".wrapper, .footer").delay(5200).queue(function( trd ) {
 			$(this).fadeIn(1000).css({'overflow' : 'auto'});
 		trd();
 		});
