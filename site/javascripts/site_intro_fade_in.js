e26a5e0cbf3ba587a6c27d76c0651f9e5a8d6928
Josh Peters <bronzegateinc@gmail.com>
2012-04-28 13:59:41 -0500

    Fixed video issues

--- site/javascripts/site_intro_fade_in.js
+++ site/javascripts/site_intro_fade_in.js
@@ -1,27 +1,21 @@
-$(document).ready(function(){
-	if($.cookie("viewed_intro") == null)
-    {
-
-		$(".lucha_home, .wrapper, .footer").css({'overflow' : 'hidden', 'display' : 'none'});
-		$(".intro_vid").delay(4600).queue(function( frst ) {
-			$(this).fadeOut("fast");	
-		frst();
-		});
-		$(".lucha_home").delay(4900).queue(function( nxt ) {
-			$(this).fadeIn(1000).css({'overflow' : 'auto', 'background-position' : 'center 24px'});
-			$('#fancybox-overlay').css({'margin-top' : '0'});
-			$(".wrapper, .footer").css({'display' : 'none'});
-		nxt();
-		});
-		$(".wrapper, .footer").delay(5200).queue(function( trd ) {
-			$(this).fadeIn(1000).css({'overflow' : 'auto'});
-		trd();
-		});
-		
-      	$.cookie('viewed_intro', true);
-    }
-    else
-    {
-    	$(".intro_vid").css({'display' : 'none'});
-    }
+$(document).ready(function(){  	
+  
+  if($.cookie("viewed_intro") == null)    {        
+    
+    $("#lucha").bind('ended', function(){				
+      $('.intro_vid').fadeOut("fast");	    
+      $(".lucha_home").fadeIn(1000).css({'overflow' : 'auto', 'background-position' : 'center 24px'});    
+      $('#fancybox-overlay').css({'margin-top' : '0'});    
+      $(".wrapper, .footer").css({'display' : 'none'});		
+      $(".wrapper, .footer").delay(500).queue(function( trd ) {			
+        $(this).fadeIn(1000).css({'overflow' : 'auto'});		
+        trd();		
+      });      	
+      $.cookie('viewed_intro', true);            
+      });    
+      }    
+      else{    	
+        $(".intro_vid").css({'display' : 'none'});            
+        $(".lucha_home, .wrapper, .footer").css({'overflow' : 'auto', 'display' : 'block'});    
+      }
 });
